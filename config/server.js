5044211eec708f07c5b2eca6e4dcf694e4521ea7
Veronica Stork <zealous.anemone@gmail.com>
2021-05-06 18:02:58 -0400

    First commit

---
+++ config/server.js
@@ -0,0 +1,9 @@
+module.exports = ({ env }) => ({
+  host: env('HOST', '0.0.0.0'),
+  port: env.int('PORT', 1337),
+  admin: {
+    auth: {
+      secret: env('ADMIN_JWT_SECRET', '77687e364ce9b31956c718b5b8adbc5f'),
+    },
+  },
+});
---
+++ extensions/users-permissions/config/jwt.js
@@ -0,0 +1,3 @@
+module.exports = {
+  jwtSecret: process.env.JWT_SECRET || 'f61acd11-fdf9-48e8-a7b9-ae05dc42ae27'
+};

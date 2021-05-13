bab540eb432d8e95c340d3554242fe90b82b0051
Veronica Stork <zealous.anemone@gmail.com>
2021-05-12 21:15:20 -0400

    Creates author page

---
+++ frontend/pages/author/[username].js
@@ -0,0 +1,43 @@
+import ArticleCard from '../../components/ArticleCard';
+
+const Author = ({ author }) => {
+  return (
+    <div className="author-page">
+      <img src={`http://localhost:1337${author.photo.url}`} />
+      <div className="author-details">
+        <h1>{author.name}</h1>
+        <p className="bio">{author.bio}</p>
+        <p>
+          @{author.twitter} - {author.website}
+        </p>
+      </div>
+    </div>
+  );
+};
+
+export async function getStaticPaths() {
+  const response = await fetch('http://localhost:1337/authors');
+  const authors = await response.json();
+  return {
+    paths: authors.map((author) => ({
+      params: {
+        username: author.username,
+      },
+    })),
+    fallback: false,
+  };
+}
+
+export async function getStaticProps({ params }) {
+  const response = await fetch(
+    `http://localhost:1337/authors?username=${params.username}`
+  );
+  const authors = await response.json();
+
+  return {
+    props: { author: authors[0] },
+    revalidate: 1,
+  };
+}
+
+export default Author;
